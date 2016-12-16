6c1d5d5ccc696b4bfa68a7467f579f6bb8982400
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2016-12-15 17:46:26 -0800

    Completed C# tests

---
+++ BlizzardProgrammingTest.Test/DeleteCharacterTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlizzardProgrammingTest.Backend.Models;
+using System.Collections.Generic;
+using BlizzardProgrammingTest.Backend;
+
+namespace BlizzardProgrammingTest.Test
+{
+    [TestClass]
+    public class DeleteCharacterTests
+    {
+        [TestMethod]
+        public void TestDeleteCharacterIdNoExist()
+        {
+            CharacterRowModel dkToon = new CharacterRowModel();
+            dkToon.Class = "Warrior";
+            dkToon.Faction = "alliance";
+            dkToon.Id = 1;
+            dkToon.Level = 1;
+            dkToon.Name = "Wally";
+            dkToon.Owner = "Masterson";
+            dkToon.Race = "Human";
+
+            List<CharacterRowModel> characterData = new List<CharacterRowModel>();
+            characterData.Add(dkToon);
+            DBObject dbo = new DBObject(null, characterData);
+
+            DBObject.DeleteCharacter(dkToon.Id + 1);
+
+            List<IDictionary<string, string>> characterList = DBObject.GetCharacterList(dkToon.Owner);
+
+            Assert.AreEqual(characterList.Count, 1);
+        }
+
+        [TestMethod]
+        public void TestDeleteCharacterIdExist()
+        {
+            CharacterRowModel dkToon = new CharacterRowModel();
+            dkToon.Class = "Warrior";
+            dkToon.Faction = "alliance";
+            dkToon.Id = 1;
+            dkToon.Level = 1;
+            dkToon.Name = "Wally";
+            dkToon.Owner = "Masterson";
+            dkToon.Race = "Human";
+
+            List<CharacterRowModel> characterData = new List<CharacterRowModel>();
+            characterData.Add(dkToon);
+            DBObject dbo = new DBObject(null, characterData);
+
+            DBObject.DeleteCharacter(dkToon.Id);
+
+            List<IDictionary<string, string>> characterList = DBObject.GetCharacterList(dkToon.Owner);
+
+            Assert.AreEqual(characterList.Count, 0);
+        }
+    }
+}
